d84fcbf3931949a0400370575256537f8918b6b1
yang <yangzhimin@dns.com>
2020-04-22 16:16:32 +0800

    commit

--- index.js
+++ index.js
@@ -36,23 +36,24 @@ exports.parse = function (jsonArray, config = {}) {
 
 let data = []
 
-const jsonify_findChildren = (children, parentId, config) => {
-    children.forEach(node => {
+const jsonify_findChildren = (data, parentId, config) => {
+    data.forEach(node => {
         node[config.parentIdField] = parentId;
-        if (Array.isArray(node[config.childrenField])) {
-            jsonify_findChildren(node[config.childrenField], node.id, config);
+        const children = node[config.childrenField] || []
+        if (config.handleNode) {
+            config.handleNode(node, children)
+        }
+        if (Array.isArray(children)) {
+            jsonify_findChildren(children, node.id, config);
             if (config.retainChildren !== true) {
                 delete node[config.childrenField]
             }
-        }
-        if (config.handleNode) {
-            config.handleNode(node, children)
         }
         data.push(node)
     })
 };
 
-exports.jsonify = function (treeData, config = {}) {
+jsonify = function (treeData, config = {}) {
     config = Object.assign({
         parentIdField: 'parentId',
         topNodeParentId: 0,
@@ -65,5 +66,44 @@ exports.jsonify = function (treeData, config = {}) {
     jsonify_findChildren(array, config.topNodeParentId, config);
     return data
 };
+
+const testData = [{
+    id: 1,
+    name: "节点1",
+    pid: null,
+    childrenList: [{
+        id: 2,
+        name: "节点1-1",
+        pid: 1,
+        childrenList: [{
+            id: 4,
+            name: "节点1-1-1",
+            pid: 2,
+        }, {
+            id: 5,
+            name: "节点1-1-2",
+            pid: 2,
+        }],
+    }, {
+        id: 3,
+        name: "节点1-2",
+        pid: 1,
+    }],
+}];
+
+const handleNode = (node, children) => {
+    node['label'] = node.name;
+    node['isLeaf'] = children.length === 0;
+}
+
+const returnData = jsonify(testData, {
+    parentIdField: 'pid',
+    topNodeParentId: null,
+    childrenField: 'childrenList',
+    retainChildren: false,
+    handleNode,
+})
+
+console.log(JSON.stringify(returnData))
 
 /** ============================================= 拆解树结构 ============================================= **/
