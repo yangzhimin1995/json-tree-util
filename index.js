64f1d6b6b7233241e3a04d306816c51bcfa298e8
yang <yangzhimin@dns.com>
2020-04-22 18:05:47 +0800

    commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-/** ============================================= 组装树结构 ============================================= **/
+/** ========================== 组装树结构 ========================== **/
 
-const parse_getChildren = (array, parentId, config) => {
+const parse_getChildren = (data, parentId, config) => {
     let returnData = [];
-    array.forEach(node => {
+    data.forEach(node => {
         if (node[config.parentIdField] === parentId) {
-            const children = parse_getChildren(array, node[config.idField], config);
+            const children = parse_getChildren(data, node[config.idField], config);
             if (config.handleNode) {
                 config.handleNode(node, children)
             }
@@ -15,7 +15,7 @@ const parse_getChildren = (array, parentId, config) => {
     return returnData;
 };
 
-exports.parse = function (jsonArray, config = {}) {
+const parse = function (jsonArray, config = {}) {
     let returnData;
     config = Object.assign({
         idField: 'id',
@@ -24,20 +24,20 @@ exports.parse = function (jsonArray, config = {}) {
         childrenField: 'children',
         handleNode: null
     }, config);
-    const array = JSON.parse(JSON.stringify(jsonArray));
-    returnData = parse_getChildren(array, config.topNodeParentId, config) || [];
+    const data = JSON.parse(JSON.stringify(jsonArray));
+    returnData = parse_getChildren(data, config.topNodeParentId, config) || [];
     return returnData
 };
 
-/** ============================================= 组装树结构 ============================================= **/
+/** ========================== 组装树结构 ========================== **/
 
 
-/** ============================================= 拆解树结构 ============================================= **/
+/** ========================== 拆解树结构 ========================== **/
 
-let data = []
+let globalData = []
 
-const jsonify_findChildren = (childrenList, parentId, config) => {
-    childrenList.forEach(node => {
+const jsonify_findChildren = (data, parentId, config) => {
+    data.forEach(node => {
         node[config.parentIdField] = parentId;
         const children = node[config.childrenField] || []
         if (config.handleNode) {
@@ -49,11 +49,11 @@ const jsonify_findChildren = (childrenList, parentId, config) => {
                 delete node[config.childrenField]
             }
         }
-        data.push(node)
+        globalData.push(node)
     })
 };
 
-exports.jsonify = function (treeData, config = {}) {
+const jsonify = function (treeData, config = {}) {
     config = Object.assign({
         parentIdField: 'parentId',
         topNodeParentId: 0,
@@ -61,10 +61,83 @@ exports.jsonify = function (treeData, config = {}) {
         retainChildren: false,
         handleNode: null
     }, config);
-    data = [];
-    const array = JSON.parse(JSON.stringify(treeData));
-    jsonify_findChildren(array, config.topNodeParentId, config);
-    return data
+    globalData = [];
+    const data = JSON.parse(JSON.stringify(treeData));
+    jsonify_findChildren(data, config.topNodeParentId, config);
+    return globalData
 };
 
-/** ============================================= 拆解树结构 ============================================= **/
+/** ========================== 拆解树结构 ========================== **/
+
+
+/** ========================== 在json数据中查找某个节点的父亲节点 ========================== **/
+
+findParentsInJson_json = (id, data, config, remainNode) => {
+    data.forEach(node => {
+        if (id === node[config.idField]) {
+            if (remainNode) {
+                globalData.unshift(node);
+            }
+            if (node[config.parentIdField] !== config.topNodeParentId) {
+                findParentsInJson_json(node[config.parentIdField], data, config, true)
+            }
+        }
+    })
+}
+
+const findParentsInJson_ids = (id, data, config, remainNode) => {
+    data.forEach(node => {
+        if (id === node[config.idField]) {
+            if (remainNode) {
+                globalData.unshift(node[config.idField]);
+            }
+            if (node[config.parentIdField] !== config.topNodeParentId) {
+                findParentsInJson_ids(node[config.parentIdField], data, config, true)
+            }
+        }
+    })
+}
+
+const findParentsInJson = function (id, data, config) {
+    config = Object.assign({
+        idField: 'id',
+        parentIdField: 'parentId',
+        topNodeParentId: 0,
+        returnType: 'ids', //ids json tree
+        remainNode: true,
+    }, config);
+    globalData = [];
+    switch (config.returnType) {
+        case "ids":
+            findParentsInJson_ids(id, data, config, config.remainNode);
+            break
+        case 'tree':
+            findParentsInJson_json(id, data, config, config.remainNode);
+            globalData = parse(globalData, config);
+            break
+        default:
+            findParentsInJson_json(id, data, config, config.remainNode);
+    }
+    return globalData
+}
+
+const testData = [
+    {nodeId: 1, name: '节点1', pid: null},
+    {nodeId: 2, name: '节点1-1', pid: 1},
+    {nodeId: 3, name: '节点1-2', pid: 1},
+    {nodeId: 4, name: '节点1-1-1', pid: 2},
+    {nodeId: 5, name: '节点1-1-2', pid: 2},
+];
+
+const a = findParentsInJson(5, testData, {
+    idField: 'nodeId',
+    parentIdField: 'pid',
+    topNodeParentId: null,
+    returnType: 'tree', //ids json tree
+})
+
+console.log(JSON.stringify(a))
+
+/** ========================== 在json数据中查找某个节点的父亲节点 ========================== **/
+
+exports = {parse, jsonify, findParentsInJson}
